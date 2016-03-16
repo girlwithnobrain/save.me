732c0350a8c527fe1dcc2a31bfd20eb776aea0df
Azereos <ariel.simulevski@gmail.com>
2016-03-16 18:37:10 +0100

    Added Main.java

---
+++ src/Client/Main.java
@@ -0,0 +1,22 @@
+package Client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        Pane root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
+    }
+}
