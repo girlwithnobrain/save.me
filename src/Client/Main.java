d9ff2459f6e0de847a5e47b1ba9e85cb19efdd61
Azer0s <sim17057@spengergasse.at>
2016-03-17 12:02:14 +0100

    Fixed Login.fxml

--- src/Client/Main.java
+++ src/Client/Main.java
@@ -7,14 +7,13 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-
     public static void main(String[] args){
         launch(args);
     }
 
     public void start(Stage stage) throws Exception {
         Pane root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        Scene scene = new Scene(root);
+        Scene scene = new Scene(root, 270, 425);
         stage.setScene(scene);
         stage.show();
         stage.setResizable(false);
